914dd4c774cae5aab8420e00701576a63a85063b
Dmitry Prudnikov <mail@polaz.com>
2026-03-25 03:04:56 +0200

    style(bench): cargo fmt

--- benches/lookup.rs
+++ benches/lookup.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
 use structured_public_domains::{is_known_suffix, lookup, registrable_domain};
 
 fn bench_lookup(c: &mut Criterion) {
